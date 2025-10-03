cd61fa4ce747a5190f17406c6eb391f9f60bc2f0
Corey Bonnell <corey.bonnell@digicert.com>
2025-10-03 15:14:42 -0400

    More tests

--- tests/test_ccadb_client.py
+++ tests/test_ccadb_client.py
@@ -1,66 +1,16 @@
-import csv
 import datetime
-import io
 
-import httpx
 from cryptography.hazmat.primitives import serialization, hashes
 
 from dossier import ccadb_client
 from tests import pki_maker
-
+from tests.ccadb_utils import (
+    create_http_client,
+    write_ccadb_all_certs,
+    write_ccadb_pems,
+)
 
 _CURRENT_TIME = datetime.datetime(2025, 1, 1, tzinfo=datetime.timezone.utc)
-
-
-def _create_http_client(pems_content: bytes, entries_content: bytes) -> httpx.Client:
-    def handle_request(request: httpx.Request) -> httpx.Response:
-        url = str(request.url)
-
-        if "NotBeforeYear=2025" in url:
-            content = pems_content
-        elif "AllCertificateRecordsCSVFormatv4" in url:
-            content = entries_content
-        else:
-            content = _EMPTY_PEMS
-
-        return httpx.Response(200, content=content)
-
-    return httpx.Client(transport=httpx.MockTransport(handle_request))
-
-
-def _write_ccadb_all_certs(entries: list[dict]) -> bytes:
-    with io.StringIO() as buffer:
-        c = csv.DictWriter(
-            buffer,
-            fieldnames=[
-                "SHA-256 Fingerprint",
-                "Revocation Status",
-                "Valid To (GMT)",
-                "Full CRL Issued By This CA",
-                "JSON Array of Partitioned CRLs",
-            ],
-            lineterminator="\n",
-        )
-        c.writeheader()
-        c.writerows(entries)
-
-        return buffer.getvalue().encode()
-
-
-def _write_ccadb_pems(entries: list[str]) -> bytes:
-    with io.StringIO() as buffer:
-        c = csv.DictWriter(
-            buffer,
-            fieldnames=["X.509 Certificate (PEM)"],
-            lineterminator="\n",
-        )
-        c.writeheader()
-        c.writerows([{"X.509 Certificate (PEM)": e} for e in entries])
-
-        return buffer.getvalue().encode()
-
-
-_EMPTY_PEMS = _write_ccadb_pems([])
 
 _ROOT = pki_maker.generate_root()
 _ICA_A_KEY_1 = pki_maker.generate_inter_a_key_1_ca(_ROOT)
@@ -74,9 +24,9 @@ _ICA_B_PEM = _ICA_B.public_bytes(serialization.Encoding.PEM).decode()
 def test_ccadb_expired_ica_skipped():
     ica = pki_maker.generate_inter_a_key_1_ca(pki_maker.generate_root())
 
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -91,7 +41,7 @@ def test_ccadb_expired_ica_skipped():
     )
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -99,9 +49,9 @@ def test_ccadb_expired_ica_skipped():
 
 
 def test_ccadb_revoked_ica_skipped():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -116,7 +66,7 @@ def test_ccadb_revoked_ica_skipped():
     )
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -124,9 +74,9 @@ def test_ccadb_revoked_ica_skipped():
 
 
 def test_ccadb_parent_revoked_ica_skipped():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -141,7 +91,7 @@ def test_ccadb_parent_revoked_ica_skipped():
     )
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -149,9 +99,9 @@ def test_ccadb_parent_revoked_ica_skipped():
 
 
 def test_ccadb_parent_valid_ica():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -166,7 +116,7 @@ def test_ccadb_parent_valid_ica():
     )
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -175,9 +125,9 @@ def test_ccadb_parent_valid_ica():
 
 
 def test_issuer_not_found():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -194,7 +144,7 @@ def test_issuer_not_found():
     ee_cert = pki_maker.generate_tls_ee(_ICA_B, pki_maker.RFC9500_INTER_B_KEY)
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -202,9 +152,9 @@ def test_issuer_not_found():
 
 
 def test_issuer_found():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM, _ICA_B_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM, _ICA_B_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -230,7 +180,7 @@ def test_issuer_found():
     ee_cert = pki_maker.generate_tls_ee(_ICA_A_KEY_1, pki_maker.RFC9500_INTER_A_KEY_1)
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
@@ -240,9 +190,9 @@ def test_issuer_found():
 
 
 def test_key_rollover_found():
-    pems_bytes = _write_ccadb_pems([_ICA_A_KEY_1_PEM, _ICA_A_KEY_2_PEM, _ICA_B_PEM])
+    pems_bytes = write_ccadb_pems([_ICA_A_KEY_1_PEM, _ICA_A_KEY_2_PEM, _ICA_B_PEM])
 
-    entries_bytes = _write_ccadb_all_certs(
+    entries_bytes = write_ccadb_all_certs(
         [
             {
                 "SHA-256 Fingerprint": (
@@ -277,7 +227,7 @@ def test_key_rollover_found():
     ee_cert = pki_maker.generate_tls_ee(_ICA_A_KEY_2, pki_maker.RFC9500_INTER_A_KEY_2)
 
     client = ccadb_client.CcadbClient(
-        _create_http_client(pems_bytes, entries_bytes),
+        create_http_client(pems_bytes, entries_bytes),
         _CURRENT_TIME,
     )
 
